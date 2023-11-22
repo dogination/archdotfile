3f2ee7d1fef3ed8f487d69009162d2046150a8c9
dogination <80304438+dogination@users.noreply.github.com>
2023-11-22 05:44:09 +0000

    Update install.py

--- install.py
+++ install.py
@@ -75,10 +75,11 @@ if pick == "4":
     os.system("chsh -s /bin/bash user")
 
 if pick == "5":
-    os.system("sudo pacman -S alacritty firefox krusader kompare unace krename lhasa unarj unrar unzip xz zip arj p7zip kde-cli-tools nemo docker libreoffice audacity handbrake blender gimp mpv obs-studio krita gwenview torbrowser-launcher transmission-gtk keepassxc ark inkscape okular kdenlive darktable kdiskmark pavucontrol veracrypt system-config-printer net-tools wine perl-image-exiftool vlc nmap whois thunderbird ghex tor proxychains metadata-cleaner bleachbit gnome-disk-utility")
+    os.system("sudo pacman -S alacritty firefox nemo docker libreoffice audacity handbrake blender gimp mpv obs-studio krita gwenview torbrowser-launcher transmission-gtk keepassxc ark inkscape okular kdenlive darktable kdiskmark pavucontrol veracrypt system-config-printer net-tools wine perl-image-exiftool vlc nmap whois thunderbird ghex tor proxychains metadata-cleaner bleachbit gnome-disk-utility")
+    os.system("krusader kompare unace krename lhasa unarj unrar unzip xz zip arj p7zip kde-cli-tools && yay -S rar")
 # arduino lmms peazip/p7zip-gui onionshare ventoy-bin rpi-imager universal-android-debloater linux-wifi-hotspot kdeconnect mpv-webm ardour lmms trimage simple-scan skanlite spectacle gnome-maps
     os.system("sudo pacman -S flatpak && flatpak install com.discordapp.Discord && yay -S flatseal")
-    os.system("yay -S rar blobsaver librewolf-bin freetube-git")
+    os.system("yay -S blobsaver librewolf-bin freetube-git")
 # minecraft-launcher lunar-client unityhub
     os.system("xdg-settings set default-web-browser librewolf.desktop")
     os.system("sudo systemctl enable tor && sudo systemctl start tor")
