4a3ef9fd2b102a9753aeeee0ab89aa1dae3d7353
dogination <80304438+dogination@users.noreply.github.com>
2024-06-22 02:56:45 +0000

    Update install.py

--- install.py
+++ install.py
@@ -62,7 +62,7 @@ if pick == "4":
     os.system("sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader")
     os.system("sudo pacman -S --needed lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader")
     os.system("sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader")
-    os.system("yay -S ani-cli memtest86-efi ix python-spotdl")
+    os.system("yay -S ani-cli memtest86-efi ix python-spotdl steamguard-cli")
     os.system("sudo pacman -S xdg-user-dirs && xdg-user-dirs-update")
     os.system('echo "-o %(title)s.%(ext)s" > ~/yt-dlp.conf')
     os.system("cp config/.alacritty.yml ~/.alacritty.yml")
